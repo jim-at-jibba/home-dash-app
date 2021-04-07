2917c35732ab42846fb8cc7e822bf1bf5e00b0fa
James Best <jim@justjibba.net>
2021-04-07 05:35:23 +0100

    Update theme colors

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,19 +1,76 @@
-import grey from "@material-ui/core/colors/grey"
 import {createMuiTheme} from "@material-ui/core/styles"
 
 const themeDark = createMuiTheme({
   palette: {
-    primary: {main: grey[200]},
-    secondary: {main: grey[400]},
-    type: "dark",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    background: {
+      paper: "#fff",
+      default: "#fafafa",
+    },
+    primary: {
+      light: "rgba(115, 232, 255, 1)",
+      main: "rgba(41, 182, 246, 1)",
+      dark: "rgba(0, 134, 195, 1)",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "rgba(93, 222, 244, 1)",
+      main: "rgba(0, 172, 193, 1)",
+      dark: "rgba(0, 124, 145, 1)",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(0, 0, 0, 0.38)",
+    },
   },
 })
 
 const themeLight = createMuiTheme({
   palette: {
-    primary: {main: grey[800]},
-    secondary: {main: grey[900]},
-    type: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    background: {
+      paper: "#fff",
+      default: "#fafafa",
+    },
+    primary: {
+      light: "rgba(115, 232, 255, 1)",
+      main: "rgba(41, 182, 246, 1)",
+      dark: "rgba(0, 134, 195, 1)",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "rgba(93, 222, 244, 1)",
+      main: "rgba(0, 172, 193, 1)",
+      dark: "rgba(0, 124, 145, 1)",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(0, 0, 0, 0.38)",
+    },
   },
 })
 
